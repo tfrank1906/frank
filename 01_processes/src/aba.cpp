f940e696b7ad821308feb6fffae056b9d6cde2bf
Tobi <frank.tobias@student.htlwrn.ac.at>
2020-09-27 17:21:45 +0200

    fertiges Miniprogramm ohne Zombie, (auch ohne waitpid() ist kein Zombi enstanden?)

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -2,10 +2,13 @@
 #include <unistd.h>
 #include <thread>
 #include <chrono>
+#include <csignal>
+#include <sys/wait.h> 
 
 using namespace std;
 
 int main()
+
 {
     auto pid{fork()};
     if (pid == 0) {
@@ -15,13 +18,21 @@ int main()
             chrono::milliseconds sleeptime(500);
             this_thread::sleep_for(sleeptime);
         }
+
     }
 
     else {
-        while (true) {
+        for (int i = 0; i < 6; i++) {
             cout << "B" << endl;
             chrono::milliseconds sleeptime(500);
             this_thread::sleep_for(sleeptime);
         }
+        
+       kill(0, SIGKILL);
+       int status;
+       waitpid(0, &status, 0);
+        
+
+        
     }
 }
