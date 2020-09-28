50d7fe15368daa743c3084e824fce0a0b69f84e2
Tobi <frank.tobias@student.htlwrn.ac.at>
2020-09-28 21:25:36 +0200

    passe Formatierung an, behebe Fehler bei kill()

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -3,34 +3,55 @@
 #include <thread>
 #include <chrono>
 #include <csignal>
-#include <sys/wait.h> 
+#include <sys/wait.h>
 
 using namespace std;
 
 int main() {
+    int status;
+    cout << "waiting for 3 seconds" << endl;
+    
     auto pid{fork()};
+    
     if (pid == 0) {
-        execl("/home/tobias/01_processes/build/charoutt", "charout",
-              "a", nullptr);
-              if(errno == 2) {
-                  cout << "Nicht erfolgreich!" << endl;
-                  quick_exit(123);
-              }
+        execl("/home/tobias/01_processes/build/charout", "charout",
+              "A", nullptr);
+             
 
+        if (errno == 2) {
+            cout << "Nicht erfolgreich!" << endl;
+          
+        }
     }
 
     else {
-        for (int i = 0; i < 6; i++) {
-            cout << "B" << endl;
-            chrono::milliseconds sleeptime(500);
-            this_thread::sleep_for(sleeptime);
+        auto pid2{fork()};
+        if (pid2 == 0) {
+            execl("/home/tobias/01_processes/build/charout", "charout",
+                  "B", nullptr);
+
+            if (errno == 2) {
+                cout << "Nicht erfolgreich!" << endl;
+            
+            }
+            
+        
         }
-
-       kill(0, SIGKILL);
-       int status;
-       waitpid(0, &status, 0);
         
+      
+        chrono::milliseconds sleeptime(3000);
+        this_thread::sleep_for(sleeptime);
 
-        
+        cout << "\nkilling both subprocesses with pids " << pid2 << " and " << pid << endl;
+        cout << "waiting for both subprocesses to be dead" << endl;
+
+        kill(pid, SIGKILL);
+        kill(pid2, SIGKILL);
+
+        waitpid(pid, &status, 0);
+        cout << "subprocess terminated w/ exit code "<< status << endl;
+
+        waitpid(pid2, &status, 0);
+        cout << "subprocess terminated w/ exit code "<< status << endl;
     }
 }
