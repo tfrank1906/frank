72c03fd2954392580a69218e888fbf3f7c422fe3
Tobi <frank.tobias@student.htlwrn.ac.at>
2020-10-05 18:04:38 +0200

    Revert "Revert "ergänze Klasse""
    This reverts commit a7eda1ff82fe13582e148235b8cd2d8ce09cc066.

--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -2,17 +2,43 @@
 #include <thread>
 using namespace std;
 
-void Golf6 () {
+void Golf6() {
     int i = 1;
-    while (true) {
+    while (true)
+    {
         cout << i << " Golf6" << endl;
         this_thread::sleep_for(1s);
         i++;
     }
 }
+class Car {
+private:
+    string carName;
+    int runden;
 
-int main() {
-    thread t{Golf6}; 
+public:
+    void operator()() {
+        int j = 1;
+        while (true)
+        {
+            cout << j << " " << carName << endl;
+            this_thread::sleep_for(1s);
+            j++;
+        }
+    }
+
+    Car(string name) {
+        this->carName = name;
+    
+    }
+
+};
+
+ int main() {
+    thread t{Golf6};
+    Car bmw("BMWE46");
+    thread t2 {ref(bmw)};
     t.join();
-    cout << t.joinable() << endl;   
+    t2.join();
+    cout << t.joinable() << endl;
 }
