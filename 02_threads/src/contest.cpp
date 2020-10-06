05eaf1c539fb430c425d61d448b24e96724b5185
Tobi <frank.tobias@student.htlwrn.ac.at>
2020-10-06 11:14:32 +0200

    ergänze Funktionalität zur Ermittlung der Gesamtzeit

--- 02_threads/src/contest.cpp
+++ 02_threads/src/contest.cpp
@@ -5,7 +5,7 @@
 #include <sstream>
 using namespace std;
 
-void Golf6() {
+int Golf6(double& alltime) {
     ostringstream bufstr;
     int i = 1;
     random_device rd;
@@ -13,21 +13,24 @@ void Golf6() {
     uniform_real_distribution<> dis{1, 10};
     double time{};
 
-    while (true)
+    while (i <= 10)
     {
         time = dis(gen);
-        bufstr << to_string(i) << " " << " Golf6 " << " " << setprecision(3)   << time << endl;
+        bufstr << to_string(i) << " " << "Golf6R" << " " << setprecision(3)   << time << endl;
         cout << bufstr.str();
         this_thread::sleep_for(chrono::milliseconds(static_cast<int>(time*1000)));      
         i++;
         bufstr.str(""); 
+        alltime += time;
     }
+    return alltime;
 }
 
 
 class Car {
 private:
     string carName;
+    double alltime{};
   public:
     void operator()() {
         ostringstream bufstr;
@@ -36,32 +39,55 @@ private:
         mt19937 gen{rd()};
         uniform_real_distribution<> dis{1, 10};
         double time{};
-    
+        
 
-        while (true)
+        while (j <= 10)
         {
             time = dis(gen);
-        
-            bufstr << to_string(j) << " " << carName << " " << setprecision(3)   << time << endl;
+            bufstr << to_string(j) << " " << carName << " " << setprecision(3)  << time << endl;
             cout << bufstr.str();
             this_thread::sleep_for(chrono::milliseconds(static_cast<int>(time*1000)));
             j++;
             bufstr.str(""); 
+            alltime += time;
         }
     }
+
 
     Car(string name) {
         this->carName = name;
     
     }
 
+    double get_total_time() {
+       return this->alltime;
+    
+    }
+
+
+
 };
 
  int main() {
-    thread t{Golf6};
+    double alltimeclass;
+    double alltime;
+
+    thread t{Golf6, ref(alltime)};
+   
+
     Car bmw("BMWE46");
+  
     thread t2 {ref(bmw)};
     t.join();
     t2.join();
-    cout << t.joinable() << endl;
+
+    alltimeclass = bmw.get_total_time();
+    
+    if (alltime > alltimeclass){
+        cout << "Sieger ist BMW E46 mit: " << alltimeclass << "s" << endl;
+        cout << "Verlierer ist Golf6 mit: " << alltime << "s" << endl;
+    } else {
+        cout << "Sieger ist Golf6 mit: " << alltime << "s" << endl;
+        cout << "Verlierer ist Golf6 mit: " << alltimeclass << "s" << endl;
+    }
 }
