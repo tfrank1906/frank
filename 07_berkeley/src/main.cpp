0f99cda79b19c5df7d9f13e6740c1aca0bbce5d8
Tobi <frank.tobias@student.htlwrn.ac.at>
2020-12-28 16:55:59 +0100

    erstelle TimeSlave - Klasse

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -1,17 +1,30 @@
 #include <iostream>
 #include <thread>
+#include <string>
 #include <chrono>
 
 #include "clock.h"
 #include "timeutils.h"
 
-
 using namespace std;
 
-int main(){
-  thread clock{Clock("testclock" , 5, 5, 5)};
-  clock.join();
+class TimeSlave{
+private:
+  string Rechnername;
+  chrono::time_point<chrono::system_clock> start_time;
 
+public:
+  TimeSlave(string Rechnername_, chrono::time_point<chrono::system_clock> start_time_){
+    this->Rechnername = Rechnername_;
+    this->start_time = start_time_;
+  }
+  void operator()(){
+ 
+  }
 }
 
-
+};
+int main(){
+  thread clock{Clock("testclock", 5, 5, 5)};
+  clock.join();
+}
