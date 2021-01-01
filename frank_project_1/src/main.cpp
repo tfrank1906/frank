991be134ddaa0b8014ba49665cf1b49b6f66a9e5
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-01 15:29:35 +0100

    erstelle schleife die eine bestimmte Anzahl an Kindtprozessen erstellt

--- frank_project_1/src/main.cpp
+++ frank_project_1/src/main.cpp
@@ -1,28 +1,40 @@
-#include <iostream> 
+#include <iostream>
 #include <unistd.h>
-#include <cerrno>    
-#include <cstdlib>   
+#include <cerrno>
+#include <cstdlib>
 #include <sys/wait.h>
+#include "CLI11.hpp"
+
 using namespace std;
-int main(){
-  auto pid{fork()};
-  if (pid == -1){
-    cerr << "forking failed : " << errno << endl;
-    exit(EXIT_FAILURE);
-  }
-  
-  if (pid == 0){
-    cout << "child process here !"<< endl;
-    cout << "child pid is " << getpid() << endl;
-    cout << "parent id is " << getppid() << endl;
-    sleep(3);
-    quick_exit(EXIT_SUCCESS);
-  } else {
-    cout << "child pid is " << pid << endl;
-    cout << "waiting for child..." << endl;
-    int status;
-    waitpid(pid, &status, 0); // 0...options
-    cout << "child terminated w / exit code " << status << endl;
-    exit(EXIT_SUCCESS);
+
+int main()
+{
+  int i = 1;
+  while (i < 5)
+  {
+
+    auto pid{fork()};
+    if (pid == -1)
+    {
+      cerr << "forking failed : " << errno << endl;
+      exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0)
+    {
+      cout << "I am Prozess: "<< i << endl;
+      cout << "parent id is " << getppid() << endl;
+      sleep(3);
+      quick_exit(EXIT_SUCCESS);
+    }
+    else
+    {
+      cout << "child pid is " << pid << endl;
+      int status;
+      waitpid(pid, &status, 0);
+      cout << "child terminated w / exit code " << status << endl;
+      
+    }
+    i++;
   }
 }
