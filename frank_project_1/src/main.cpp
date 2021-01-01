dc621fa7905c8d47139c67bf3f024dd1ccfe9364
Tobi <frank.tobias@student.htlwrn.ac.at>
2021-01-01 16:15:39 +0100

    refaktorisiere output für mehr Übersichtlichkeit

--- frank_project_1/src/main.cpp
+++ frank_project_1/src/main.cpp
@@ -7,33 +7,35 @@
 
 using namespace std;
 
-int main()
-{
-  int i = 1;
-  while (i < 5)
-  {
+void print_process(){
+  cout << "I am Prozess: " << getpid() << endl;
+  cout << "parent id is " << getppid() << endl;
+  
+}
 
+int main(){
+
+  int i = 1;
+
+
+  while (i < 3)
+  {
     auto pid{fork()};
-    if (pid == -1)
-    {
+    if (pid == -1){
       cerr << "forking failed : " << errno << endl;
       exit(EXIT_FAILURE);
     }
 
-    if (pid == 0)
-    {
-      cout << "I am Prozess: "<< i << endl;
-      cout << "parent id is " << getppid() << endl;
-      sleep(3);
+    if (pid == 0){
+      print_process();
+      sleep(1);
       quick_exit(EXIT_SUCCESS);
     }
-    else
-    {
-      cout << "child pid is " << pid << endl;
+    else {
+    
       int status;
       waitpid(pid, &status, 0);
-      cout << "child terminated w / exit code " << status << endl;
-      
+      //cout << "child terminated w / exit code " << status << endl;
     }
     i++;
   }
